df66a5cac5bd3f8b106e671b47be92c10908a794
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-04-30 15:39:07 -0300

    import LoginForm component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,14 @@
+import { LoginForm } from "../components/login/LoginForm";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <main className="flex flex-col items-center justify-center gap-8 min-h-svh font-poppins">
+      <h1 className="text-7xl md:text-8xl text-latinBlue">
+        LATIN<span className="font-bold text-black">AD</span>
+      </h1>
+      <section className="w-full max-w-md px-4 ">
+        <LoginForm />
+      </section>
+    </main>
+  );
 };
