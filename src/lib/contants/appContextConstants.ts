d3c3f77105b6265fbd58e1554f47fd4606f3b77b
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-04-30 17:46:17 -0300

    create defult context values constants

---
+++ src/lib/contants/appContextConstants.ts
@@ -0,0 +1,11 @@
+import { AppContextValues } from "../interfaces";
+
+
+export const DEFAULT_APP_CONTEXT_VALUES: AppContextValues = {
+  userCredentials: {
+    email: '',
+    token: '',
+    isLogged: false
+  },
+  setUserCredentials: () => { }
+}
