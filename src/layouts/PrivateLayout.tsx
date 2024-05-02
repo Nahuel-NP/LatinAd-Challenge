d09afb0bbc5501cfe9cc81873e3ef2e8514e9b17
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-02 14:33:37 -0300

    add Navbar to private layout

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -1,9 +1,18 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import { Navbar } from "../components/Navbar";
 
 export const PrivateLayout = () => {
   const { userCredentials } = useContext(AppContext);
 
-  return userCredentials.isLogged ? <Outlet /> : <Navigate to="/login" />;
+  if (userCredentials.isLogged)
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    );
+
+  return <Navigate to="/login" />;
 };
