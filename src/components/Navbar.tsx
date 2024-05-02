04b48dfb59b2170ae729cb060be7ea3162a691d8
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-02 20:45:28 -0300

    change navbar colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,14 +10,14 @@ export const Navbar = () => {
     saveCredentials(DEFAULT_APP_CONTEXT_VALUES.userCredentials);
   };
   return (
-    <nav>
+    <nav className="fixed w-full bg-latinBlue">
       <div className="container flex items-center justify-between p-4 mx-auto font-poppins">
-        <Link to="/" className="text-2xl uppercase text-latinBlue">
-          Latin<span className="font-bold text-black">Ad</span>
+        <Link to="/" className="text-2xl uppercase text-balck">
+          Latin<span className="font-bold text-white">Ad</span>
         </Link>
         <button
           onClick={logout}
-          className="px-4 py-2 text-white transition-colors rounded-md hover:bg-blue-600 bg-latinBlue"
+          className="px-4 py-2 text-black transition-colors bg-white rounded-md hover:bg-blue-600"
         >
           Cerrar Sessión
         </button>
