909b4110939e574f8d795c0614288e4414a9e2ab
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-03 13:32:50 -0300

    add dodger-blue colors palete

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ export default {
         poppins: ["poppins", "sans-serif"],
       },
       colors: {
-        latinBlue: "#1890ff",
+        'dodger-blue': {
+          '50': '#edfaff',
+          '100': '#d7f1ff',
+          '200': '#b9e8ff',
+          '300': '#88ddff',
+          '400': '#50c7ff',
+          '500': '#28a9ff',
+          '600': '#1890ff',
+          '700': '#0a73eb',
+          '800': '#0f5cbe',
+          '900': '#135095',
+          '950': '#11315a',
+      },
       },
       gridTemplateRows:{
         wrapper:"auto 1fr auto"
