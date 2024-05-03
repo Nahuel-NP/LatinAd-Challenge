8784008010553e2bfe629ca6774a6a55e3531d69
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-03 16:50:50 -0300

    add QUERY_KEY constant

--- src/lib/contants/sharedConstants.ts
+++ src/lib/contants/sharedConstants.ts
@@ -1,4 +1,6 @@
 
 export const BASE_URL = "https://challenge-front-7fw1.onrender.com";
 
-export const PER_PAGE_VALUES = [3, 6, 12, 24];
+export const PER_PAGE_VALUES = [ 6, 12, 24];
+
+export const QUERY_KEY = "displays";
