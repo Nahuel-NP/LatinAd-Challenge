dba320b8c06c11ec597a50d43655af048f43d9ba
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-03 13:33:27 -0300

    restyle login

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -4,10 +4,11 @@ import { LoginResponse } from "../../lib/interfaces";
 import { BASE_URL } from "../../lib/contants";
 import { AppContext } from "../../context/AppContext";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 export const LoginForm = () => {
   const [showPassword, setShowPassword] = useState(false);
-
+  const [loading, setLoading] = useState(false);
   const { saveCredentials } = useContext(AppContext);
 
   const navigate = useNavigate();
@@ -19,7 +20,7 @@ export const LoginForm = () => {
   const login = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.currentTarget;
-
+    setLoading(true);
     const { email, password } = event.target as HTMLFormElement;
 
     fetch(`${BASE_URL}/login`, {
@@ -39,15 +40,16 @@ export const LoginForm = () => {
         navigate("/");
       })
       .catch((error) => {
-        console.log(error);
-      });
+        toast.error("Email o contraseña incorrecto");
+      })
+      .finally(() => setLoading(false));
 
     form.reset();
   };
 
   return (
     <form
-      className="flex flex-col items-start gap-6 bg-[#62a8ea] py-12 px-6"
+      className="flex flex-col items-start gap-6 px-6 py-12"
       onSubmit={login}
     >
       <div className="flex flex-col w-full gap-2">
@@ -74,7 +76,7 @@ export const LoginForm = () => {
           minLength={4}
           className="px-4 py-2 rounded-sm"
         />
-        <label className="text-white">
+        <label className="flex items-center text-white">
           <input
             type="checkbox"
             onChange={toggleShowPassword}
@@ -83,8 +85,8 @@ export const LoginForm = () => {
           <span className="text-sm">Mostar contraseña</span>
         </label>
       </div>
-      <button className="px-4 py-2 mt-4 font-semibold transition-colors bg-white rounded-sm hover:text-white hover:bg-latinBlue ">
-        Ingresar
+      <button disabled={loading} className="px-6 py-2 mt-2 font-semibold text-white transition-colors rounded-full disabled:pointer-events-none disabled:bg-gray-400 bg-dodger-blue-600 hover:text-dodger-blue-700 hover:bg-white ">
+        {loading ? "Ingresando" : "Ingresar"}
       </button>
     </form>
   );
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,23 @@
+import { Toaster } from "sonner";
 import { LoginForm } from "../components/login/LoginForm";
 
 export const LoginPage = () => {
   return (
-    <main className="flex flex-col items-center justify-center gap-8 min-h-svh font-poppins">
-      <h1 className="text-7xl md:text-8xl text-latinBlue">
-        LATIN<span className="font-bold text-black">AD</span>
-      </h1>
+    <section
+      className="flex flex-col items-center justify-center gap-8 bg-dodger-blue-950 min-h-svh font-poppins"
+    >
+      <div className="relative">
+        <h1 className="text-white text-7xl md:text-8xl text-latinBlue">
+          LATIN<span className="font-bold text-dodger-blue-600">AD</span>
+        </h1>
+        <p className="absolute right-0 px-2 text-base font-bold text-center text-black uppercase bg-orange-500 rounded-full -rotate-3">
+          Challenge
+        </p>
+      </div>
       <section className="w-full max-w-md px-4 ">
         <LoginForm />
       </section>
-    </main>
+      <Toaster position="bottom-center" richColors />
+    </section>
   );
 };
