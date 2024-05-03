3e3571f106052287d7636447af8840ee5cadb875
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-03 16:42:16 -0300

    change queyclient provider ubication

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -2,19 +2,15 @@ import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { Navbar } from "../components/Navbar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const PrivateLayout = () => {
   const { userCredentials } = useContext(AppContext);
 
   if (userCredentials.isLogged) {
-    const queryClient = new QueryClient();
     return (
       <main className="relative min-h-screen">
         <Navbar />
-        <QueryClientProvider client={queryClient}>
-          <Outlet />
-        </QueryClientProvider>
+        <Outlet />
       </main>
     );
   }
