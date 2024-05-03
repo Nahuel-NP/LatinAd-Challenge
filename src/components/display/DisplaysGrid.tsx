b9db660c71ff4979dc8912d63642f14a89bb519d
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-03 13:37:19 -0300

    add not result message

--- src/components/display/DisplaysGrid.tsx
+++ src/components/display/DisplaysGrid.tsx
@@ -6,7 +6,13 @@ interface Props {
 }
 export const DisplaysGrid = ({ data }: Props) => {
   return (
-    <section className="grid gap-4 border-gray-400 md:grid-cols-2 xl:grid-cols-3">
+    <section className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
+      {!data.length && (
+        <p className="self-center text-xl font-semibold text-center md:col-span-2 xl:col-span-3 text-dodger-blue-960">
+          {" "}
+          Sin Resultados
+        </p>
+      )}
       {data.map((display, i) => (
         <DisplayCard {...display} key={display.name + i} />
       ))}
