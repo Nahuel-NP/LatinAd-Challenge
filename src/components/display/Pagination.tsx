e82e654045d3313ffa04db113b32123dd1430203
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-03 13:47:24 -0300

    add aria-label to pagination buttons

--- src/components/display/Pagination.tsx
+++ src/components/display/Pagination.tsx
@@ -25,6 +25,7 @@ export const Pagination = ({ totalCount }: Props) => {
         <ul className="flex items-center h-10 -space-x-px text-base [&>li>button]:transition-colors [&>li>button]:border [&>li>button]:bg-white [&>li>button]:border-dodger-blue-800  [&>li>span]:bg-white">
           <li>
             <button
+            aria-label="anterior"
               disabled={filters.page === 1}
               onClick={() => changePage(filters.page - 1)}
               className="flex items-center justify-center h-10 px-4 disabled:pointer-events-none disabled:bg-gray-200 rounded-s-lg hover:bg-dodger-blue-600 hover:text-white "
@@ -91,6 +92,7 @@ export const Pagination = ({ totalCount }: Props) => {
 
           <li>
             <button
+            aria-label="siguiente"
               onClick={() => changePage(filters.page + 1)}
               disabled={filters.page === lastPage || lastPage === 0}
               className="flex items-center justify-center h-10 px-4 disabled:pointer-events-none disabled:bg-gray-200 rounded-e-lg hover:bg-dodger-blue-600 hover:text-white "
