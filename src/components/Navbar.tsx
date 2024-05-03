e5d77aa608d7b1435933a1bed5153855c97fb9c9
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-03 13:36:36 -0300

    restyle navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,14 +10,14 @@ export const Navbar = () => {
     saveCredentials(DEFAULT_APP_CONTEXT_VALUES.userCredentials);
   };
   return (
-    <nav className="fixed w-full bg-latinBlue">
+    <nav className="fixed w-full bg-dodger-blue-950">
       <div className="container flex items-center justify-between p-4 mx-auto font-poppins">
-        <Link to="/" className="text-2xl uppercase text-balck">
-          Latin<span className="font-bold text-white">Ad</span>
+        <Link to="/" className="text-2xl font-semibold text-white uppercase">
+          Latin<span className="font-bold text-dodger-blue-600">Ad</span>
         </Link>
         <button
           onClick={logout}
-          className="px-4 py-2 text-black transition-colors bg-white rounded-md hover:bg-blue-600"
+          className="px-4 py-2 font-semibold text-white transition-colors border rounded-full border-dodger-blue-800 bg-dodger-blue-800 hover:bg-dodger-blue-700"
         >
           Cerrar Sessión
         </button>
