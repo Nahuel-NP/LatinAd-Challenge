a7de8a77db4996959242fa523160ed216a124266
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-03 13:36:57 -0300

    restyle product card

--- src/components/display/DisplayCard.tsx
+++ src/components/display/DisplayCard.tsx
@@ -2,26 +2,40 @@ import { Display } from "../../lib/interfaces";
 
 export const DisplayCard = (display: Display) => {
   return (
-    <section className="flex flex-col max-w-md overflow-hidden border border-gray-400 rounded-lg shadow-sm">
-      <div className="flex w-full ">
+    <section className="grid grid-cols-3 grid-rows-[1fr_auto_auto] gap-2 p-4  bg-white  rounded-xl  max-w-sm mx-auto">
+      <div className="overflow-hidden rounded-lg ">
         <img
-          className="object-cover w-1/3"
+          className="object-cover w-full h-full"
           src={display.picture_url}
-          alt={`imagen de ${display.name}`}
+          alt={display.name}
         />
+      </div>
 
-        <div className="flex flex-col w-2/3 gap-2 p-4 text-sm md:p-4">
-          <h3 className="text-xl font-bold text-gray-800">{display.name}</h3>
-
-          <p className="text-gray-600 ">{display.description}</p>
-          <p className="flex items-center gap-2 ">
-            Dimensiones: {display.resolution_width} x{" "}
-            {display.resolution_height}
-          </p>
-
-          <p className="">Tipo: {display.type}</p>
-          <p className="">Precio por dia: ${display.price_per_day}</p>
-        </div>
+      <div className="relative col-span-2  p-2 rounded-lg text-dodger-blue-950 [&>p>span]:font-semibold [&>p]:text-gray-600">
+        <h3 className="text-xl font-bold ">{display.name}</h3>
+        <p>
+          {" "}
+          <span>Tipo:</span> {display.type}
+        </p>
+        <p>
+          <span>Resolución:</span> {display.resolution_width} x{" "}
+          {display.resolution_height}
+        </p>
+        <p>
+          <span>Precio x día:</span> ${display.price_per_day}
+        </p>
+      </div>
+      <div className="col-span-3 p-2 border-b">
+        <p className="text-gray-600 line-clamp-2">
+          <span className="font-semibold text-dodger-blue-950">
+            Descripción:
+          </span>{" "}
+          {display.description} ho ho asd asdasldkas asjdkas asdasd asd asd asdas dasdasd asd 
+        </p>
+      </div>
+      <div className="flex justify-end col-span-3 gap-8 uppercase rounded-lg">
+        <button className="font-semibold text-orange-500 transition-transform hover:scale-105">Editar</button>
+        <button className="font-semibold transition-transform text-dodger-blue-600 hover:scale-105">Ver Más</button>
       </div>
     </section>
   );
