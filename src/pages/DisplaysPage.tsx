ba09fda1c514e44607c4759fcd8c75cc0e6cb120
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-03 22:05:36 -0300

    expand and collapse create form

--- src/pages/DisplaysPage.tsx
+++ src/pages/DisplaysPage.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { DisplaysGrid } from "../components/display/DisplaysGrid";
 import { BASE_URL, QUERY_KEY } from "../lib/contants";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 import { DisplayResponse } from "../lib/interfaces";
 import { SearchBar } from "../components/display/SearchBar";
@@ -48,14 +48,30 @@ export const Displays = () => {
     staleTime: 60 * 1000 * 10,
   });
 
+  const [showForm, setShowForm] = useState(false);
+  const toggleShowForm = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <section className="flex flex-col pt-[72px] bg-gray-100 min-h-svh">
-      <div className="container grid flex-1 grid-rows-1 p-4 mx-auto lg:grid-cols-7">
-        <aside className="p-4 lg:col-span-2 ">
-          <h2 className="text-xl font-semibold text-center border-b">
-            Nuevo Display
-          </h2>
-          <CreateDisplayForm />
+      <div className="container grid flex-1 p-4 mx-auto lg:grid-cols-7">
+        <aside className="flex flex-col items-center p-4 lg:col-span-2">
+          <h2 className="mb-4 text-xl text-center">Crear nuevo display</h2>
+          <button
+            onClick={toggleShowForm}
+            className="px-4 py-2 my-2 text-white bg-orange-500 rounded-lg justify-self-center lg:hidden"
+          >
+            {showForm ? "Ocultar formulario" : "Ver formulario"}
+          </button>
+          <div
+            style={{ transition: "max-height ease-in 0.3s" }}
+            className={`overflow-hidden max-h-0 lg:max-h-full  ${
+              showForm ? "max-h-full" : "max-h-0 lg:max-h-full"
+            }`}
+          >
+            <CreateDisplayForm />
+          </div>
         </aside>
         <div className="grid border-t-2 border-gray-300 lg:border-l-2 lg:border-t-0 lg:col-span-5 grid-rows-wrapper">
           <SearchBar />
@@ -73,6 +89,13 @@ export const Displays = () => {
 
           {!isFetching && !isError && (
             <Pagination totalCount={data?.totalCount!} />
+          )}
+          {isError && (
+            <div className="grid w-full h-full place-items-center">
+              <p className="text-2xl font-bold text-dodger-blue-950">
+                Ocurrio un Error
+              </p>
+            </div>
           )}
         </div>
       </div>
