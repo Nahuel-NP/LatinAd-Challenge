fb03285f1262211dbccb12dd429ee96c1891dc6a
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-05-04 14:02:16 -0300

    fix media query grid-columns

--- src/components/display/DisplaysGrid.tsx
+++ src/components/display/DisplaysGrid.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 export const DisplaysGrid = ({ data }: Props) => {
   return (
-    <section className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
+    <section className="grid gap-4 md:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3">
       {!data.length && (
         <p className="self-center text-xl font-semibold text-center md:col-span-2 xl:col-span-3 text-dodger-blue-960">
           {" "}
